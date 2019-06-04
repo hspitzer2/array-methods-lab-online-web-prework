ca6037a2a72f8493368584de992d398fe542ce7f
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-04 23:29:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,7 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  element1 = "wow"
+  element2 = "Tardis"
   array.include?(element1)
     return
   array.include?(element2)
