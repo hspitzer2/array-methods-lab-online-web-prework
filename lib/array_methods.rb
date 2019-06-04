7057f44a5111e522dc357452bacd2a174afc9ee6
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-04 23:30:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,8 @@
-def using_include(array, element)
+def using_include(array, element[])
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element1 = "wow"
-  element2 = "Tardis"
-  array.include?(element1)
-    return
-  array.include?(element2)
-    return
+  array.include?(element)
+  
+  
 end
 
 def using_sort(array)
